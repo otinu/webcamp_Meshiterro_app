7c03e55df92fd81f1abb2879d6e07e838b60d20b
otinu <fitshter@gmail.com>
2021-03-23 12:12:35 +0000

    [Finish] 4-6

--- meshiterro/app/models/post_image.rb
+++ meshiterro/app/models/post_image.rb
@@ -6,10 +6,12 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   #ここから手動で追記(4章)===============================
   has_many :favorites, dependent: :destroy
-
+  #ここから手動で追記(6章)===============================
+  validates :shop_name, presence: true
+  validates :image, presence: true
+  #ここから手動で追記(4章)===============================
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-
   #ここまで手動で追記===============================
 end
