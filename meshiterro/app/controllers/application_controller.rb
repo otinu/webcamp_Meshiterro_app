2f8f20468ba44b852668aa39a2dbf1d0ed10bad8
otinu <fitshter@gmail.com>
2021-03-25 10:21:29 +0000

    [Fix] sign_up

--- meshiterro/app/controllers/application_controller.rb
+++ meshiterro/app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
 
 #ここから手動で追記======================================================
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top] #ログインされてない場合はTop『アクション』が実行。つまり、ここでは、「after_sign_in_path_for(resource)を実行？
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(resource) #URL「(省略)/users/sign_in」の後は、post_images_pathへリダイレクト。おそらく、URLのusersの部分は記述しなくてもdeviseの機能で自動追記される。
     post_images_path
   end
+
 
   protected
 
